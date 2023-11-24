8672bce5e3579bdf5ab6c3d4ece9a30648412ee0
Hannele Ruiz <justlemoncl@gmail.com>
2023-11-24 03:38:08 -0300

    Added check to make sure that the properties exist

--- WhatsPlaying/Script.cs
+++ WhatsPlaying/Script.cs
@@ -60,14 +60,17 @@ public class WhatsPlaying : Script
     {
         bool isUsingVehicle = Game.Player.Character.CurrentVehicle != null;
         bool isFirstPerson = Function.Call<int>(isUsingVehicle ? Hash.GET_FOLLOW_VEHICLE_CAM_VIEW_MODE : Hash.GET_FOLLOW_PED_CAM_VIEW_MODE) == 4;
+
+        string title = properties == null ? "" : properties.Title;
+        string artist = properties == null ? "" : properties.Artist;
         
         currentMedia.Visible = (config.UiShowOnFirstPerson && isFirstPerson) || (config.UiShowOnFoot && !isUsingVehicle) || (!isFirstPerson && isUsingVehicle);
-        currentMedia.Title = properties.Title;
-        currentMedia.Artist = properties.Artist;
+        currentMedia.Title = title;
+        currentMedia.Artist = artist;
         
         pool.Process();
         
-        dashboard.SetRadio("", "Media Player", properties.Artist, properties.Title);
+        dashboard.SetRadio("", "Media Player", artist, title);
     }
 
     #endregion
