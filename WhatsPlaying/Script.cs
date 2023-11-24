83c237dc8b2023cb5a5dca51560454b48d64434f
Hannele Ruiz <justlemoncl@gmail.com>
2023-11-24 01:26:02 -0300

    Added basic script

---
+++ WhatsPlaying/Script.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GTA;
+
+namespace WhatsPlaying;
+
+/// <summary>
+/// Script that shows what is currently playing in your vehicle.
+/// </summary>
+public class WhatsPlaying : Script
+{
+    #region Constructors
+
+    /// <summary>
+    /// Creates a new script.
+    /// </summary>
+    public WhatsPlaying()
+    {
+        Tick += OnTick;
+    }
+    
+    #endregion
+    
+    #region Event Functions
+
+    private async void OnTick(object sender, EventArgs e)
+    {
+    }
+
+    #endregion
+}
