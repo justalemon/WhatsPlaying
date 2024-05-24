8494e1e52a587291defaa54f1b6a6d81706a0bdd
Hannele Ruiz <justlemoncl@gmail.com>
2024-05-24 09:49:41 -0400

    Added enum to define which corner to use

---
+++ WhatsPlaying/Corner.cs
@@ -0,0 +1,24 @@
+﻿namespace WhatsPlaying;
+
+/// <summary>
+/// Represents a corner on the screen.
+/// </summary>
+public enum Corner
+{
+    /// <summary>
+    /// The Top-Left corner.
+    /// </summary>
+    TopLeft,
+    /// <summary>
+    /// The Top-Right corner.
+    /// </summary>
+    TopRight,
+    /// <summary>
+    /// The Bottom-Left corner.
+    /// </summary>
+    BottomLeft,
+    /// <summary>
+    /// The Bottom-Right corner.
+    /// </summary>
+    BottomRight
+}
